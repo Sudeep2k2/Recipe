54e221724e5fcb090a5a4a3cd7792adaf3be8938
Sudeep2k2 <rockingsudeep08@gmail.com>
2024-11-24 23:11:18 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Include the HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // Include JSX and TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
